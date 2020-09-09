e2b671b6268683129262b14f74a395da1470351d
Paul Richard <paulrpm@gmail.com>
2020-09-09 19:54:36 -0300

    created basic migrations

---
+++ db/migrations/20200909222440_tabela_pokemon.php
@@ -0,0 +1,40 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class TabelaPokemon extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    addCustomColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Any other destructive changes will result in an error when trying to
+     * rollback the migration.
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $table = $this->table('pokemon', ['id' => true]);
+        $table->addColumn('nome', 'string')
+            ->addColumn('tipo_1', 'integer', ['null' => true])
+            ->addColumn('tipo_2', 'integer', ['null' => true])
+            ->create();
+    }
+}
