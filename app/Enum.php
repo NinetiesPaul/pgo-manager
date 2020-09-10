e9b771078539156762117a2f2937ad4bf0670529
paulrpm@gmail.com <52545658git>
2020-09-10 10:42:50 -0300

    created enum class

---
+++ app/Enum.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App;
+
+
+class Enum
+{
+    const TYPE_NORMAL_ID = 1;
+    const TYPE_GRASS_ID = 2;
+    const TYPE_FIRE_ID = 3;
+    const TYPE_WATER_ID = 4;
+    const TYPE_FIGHTING_ID = 5;
+    const TYPE_FLYING_ID = 6;
+    const TYPE_POISON_ID = 7;
+    const TYPE_GROUND_ID = 8;
+    const TYPE_ROCK_ID = 9;
+    const TYPE_ICE_ID = 10;
+    const TYPE_FAIRY_ID = 11;
+    const TYPE_STEEL_ID = 12;
+    const TYPE_PSYCHIC_ID = 13;
+    const TYPE_DARK_ID = 14;
+    const TYPE_ELECTRIC_ID = 15;
+    const TYPE_DRAGON_ID = 16;
+    const TYPE_BUG_ID = 17;
+    const TYPE_GHOST_ID = 18;
+}
