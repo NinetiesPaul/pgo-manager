c1fff72ca3a7c396705fe85f7e02805196468e36
Paul Richard <paulrpm@gmail.com>
2020-09-12 20:39:47 -0300

    added route to new reading endpoints

--- routes.php
+++ routes.php
@@ -2,7 +2,7 @@
 
 use Pecee\SimpleRouter\SimpleRouter;
 use App\Controllers\IndexController;
-use App\Controllers\DataController;
+use App\Controllers\ReaderController;
 
 SimpleRouter::get('/', function() {
     $index = new IndexController();
@@ -10,6 +10,21 @@ SimpleRouter::get('/', function() {
 });
 
 SimpleRouter::get('/reader', function() {
-    $index = new IndexController();
-    $index->reader();
+    $reader = new ReaderController();
+    $reader->reader();
+});
+
+SimpleRouter::get('/populate_pokemons', function() {
+    $reader = new ReaderController();
+    $reader->populate_pokemons();
+});
+
+SimpleRouter::get('/populate_quick', function() {
+    $reader = new ReaderController();
+    $reader->populate_quick();
+});
+
+SimpleRouter::get('/populate_charge', function() {
+    $reader = new ReaderController();
+    $reader->populate_charge();
 });
