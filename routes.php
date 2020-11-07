2ac859ea85c4f579a2676f77e66f05856b7fa7ca
Paul Richard <paulrpm@gmail.com>
2020-11-07 16:36:26 -0300

    added new route; route refactoring

--- routes.php
+++ routes.php
@@ -1,15 +1,9 @@
 <?php
 
 use Pecee\SimpleRouter\SimpleRouter;
-use App\Controllers\IndexController;
 use App\Controllers\Controller;
 
 SimpleRouter::get('/', function() {
-    $index = new IndexController();
-    $index->index();
-});
-
-SimpleRouter::get('/reader', function() {
     $reader = new Controller();
     $reader->teamBuilder();
 });
@@ -22,4 +16,9 @@ SimpleRouter::get('/pokedb', function() {
 SimpleRouter::get('/getPokemon/{name}', function($name) {
     $reader = new Controller();
     $reader->getPokemon($name);
+});
+
+SimpleRouter::post('/pkmpvp', function() {
+    $reader = new Controller();
+    $reader->storePkmPvp();
 });
