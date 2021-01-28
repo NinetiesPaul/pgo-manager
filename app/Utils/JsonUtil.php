131ecdaffdf139e595fb87f1302db8d4fa257707
paulrpm@gmail.com <52545658git>
2021-01-28 13:11:05 -0300

    json refactoring; now json is creating main local database information

--- app/Utils/JsonUtil.php
+++ app/Utils/JsonUtil.php
@@ -10,71 +10,50 @@ class JsonUtil
      * This class is responsible for retrieving auxiliary JSON content
      */
 
-    const SHADOW_JSON = 'includes/files/shadow_pokemon.json';
-    const POKEMON_NAMES_JSON = 'includes/files/pokemon_names.json';
-    const GALARIAN_JSON = 'includes/files/galarian_pokemon.json';
-    const ALOLAN_JSON = 'includes/files/alolan_pokemon.json';
     const MEGA_JSON = 'includes/files/mega_pokemon.json';
     const TYPES_JSON = 'includes/files/pokemon_types.json';
     const TYPE_EFFECTIVENESS_JSON = 'includes/files/type_effectiveness.json';
     const STATS_JSON = 'includes/files/pokemon_stats.json';
-    const CP_MULTIPLIER_JSON = 'includes/files/cp_multiplier.json';
     const QUICK_MOVES_JSON = 'includes/files/fast_moves.json';
     const CHARGE_MOVES_JSON = 'includes/files/charged_moves.json';
+    const CURRENT_PKM_MOVES_JSON = 'includes/files/current_pokemon_moves.json';
+    
+    const ALLOWED_FORMS = [
+        'Altered', 'Origin', 'Galarian', 'Shadow', 'Alolan', 'Defense', 'Attack', 'Speed'
+    ];
 
-    public static function getShadowPokemons()
+    public static function getMegaPokemons($force = false)
     {
-        if (!file_exists(self::SHADOW_JSON)) {
-            file_put_contents(self::SHADOW_JSON, file_get_contents("https://pogoapi.net/api/v1/shadow_pokemon.json"));
-        }
-
-        $content = file_get_contents(self::SHADOW_JSON);
-        return json_decode($content, true);
-    }
-
-    public static function getPokemonsNames()
-    {
-        if (!file_exists(self::POKEMON_NAMES_JSON)) {
-            file_put_contents(self::POKEMON_NAMES_JSON, file_get_contents("https://pogoapi.net/api/v1/pokemon_names.json"));
-        }
-
-        $content = file_get_contents(self::POKEMON_NAMES_JSON);
-        return json_decode($content, true);
-    }
-
-    public static function getGalarianPokemons()
-    {
-        if (!file_exists(self::GALARIAN_JSON)) {
-            file_put_contents(self::GALARIAN_JSON, file_get_contents("https://pogoapi.net/api/v1/galarian_pokemon.json"));
-        }
-
-        $content = file_get_contents(self::GALARIAN_JSON);
-        return json_decode($content, true);
-    }
-
-    public static function getAlolanPokemons()
-    {
-        if (!file_exists(self::ALOLAN_JSON)) {
-            file_put_contents(self::ALOLAN_JSON, file_get_contents("https://pogoapi.net/api/v1/alolan_pokemon.json"));
-        }
-
-        $content = file_get_contents(self::ALOLAN_JSON);
-        return json_decode($content, true);
-    }
-
-    public static function getMegaPokemons()
-    {
-        if (!file_exists(self::MEGA_JSON)) {
+        if (!file_exists(self::MEGA_JSON) || $force) {
             file_put_contents(self::MEGA_JSON, file_get_contents("https://pogoapi.net/api/v1/mega_pokemon.json"));
+            $read = file_get_contents("https://pogoapi.net/api/v1/mega_pokemon.json");
+            $read = json_decode($read, true);
+
+            $toWrite = [];
+            foreach ($read as $item) {
+
+                $pokemon = [
+                    'stats' => [
+                        'atk' => $item['stats']['base_attack'],
+                        'def' => $item['stats']['base_defense'],
+                        'sta' => $item['stats']['base_stamina'],
+                    ],
+                    'type' => (count($item['type']) > 1) ? implode("/", $item['type']) : $item['type'][0],
+                ];
+
+                $toWrite[$item['mega_name']] = $pokemon;
+            }
+
+            file_put_contents(self::MEGA_JSON, json_encode($toWrite, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
         }
 
         $content = file_get_contents(self::MEGA_JSON);
         return json_decode($content, true);
     }
 
-    public static function getTypeEffectiveness()
+    public static function getTypeEffectiveness($force = false)
     {
-        if (!file_exists(self::TYPE_EFFECTIVENESS_JSON)) {
+        if (!file_exists(self::TYPE_EFFECTIVENESS_JSON) || $force) {
             file_put_contents(self::TYPE_EFFECTIVENESS_JSON, file_get_contents("https://pogoapi.net/api/v1/type_effectiveness.json"));
         }
 
@@ -82,57 +61,120 @@ class JsonUtil
         return json_decode($content, true);
     }
 
-    public static function getType()
+    public static function getType($force = false)
     {
-        if (!file_exists(self::TYPES_JSON)) {
-            file_put_contents(self::TYPES_JSON, file_get_contents("https://pogoapi.net/api/v1/pokemon_types.json"));
+        if (!file_exists(self::TYPES_JSON) || $force) {
+            $read = file_get_contents("https://pogoapi.net/api/v1/pokemon_types.json");
+            $read = json_decode($read, true);
+
+            $toWrite = [];
+            foreach ($read as $item) {
+
+                $name = $item['pokemon_name'];
+
+                if (in_array($item['form'], self::ALLOWED_FORMS)) {
+                    $name = $item['form'] . " " . $item['pokemon_name'];
+                }
+
+                $toWrite[$name] = (count($item['type']) > 1) ? implode("/", $item['type']) : $item['type'][0];
+            }
+
+            file_put_contents(self::TYPES_JSON, json_encode($toWrite, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
         }
 
         $content = file_get_contents(self::TYPES_JSON);
         return json_decode($content, true);
     }
 
-    public static function getStats()
+    public static function getStats($force = false)
     {
-        if (!file_exists(self::STATS_JSON)) {
-            file_put_contents(self::STATS_JSON, file_get_contents("https://pogoapi.net/api/v1/pokemon_stats.json"));
+        if (!file_exists(self::STATS_JSON) || $force) {
+            $read = file_get_contents("https://pogoapi.net/api/v1/pokemon_stats.json");
+            $read = json_decode($read, true);
+
+            $toWrite = [];
+            foreach ($read as $item) {
+
+                $name = $item['pokemon_name'];
+
+                if (in_array($item['form'], self::ALLOWED_FORMS)) {
+                    $name = $item['form'] . " " . $item['pokemon_name'];
+                }
+
+                $toWrite[$name] = [
+                    'atk' => $item['base_attack'],
+                    'def' => $item['base_defense'],
+                    'sta' => $item['base_stamina'],
+                    'id' => $item['pokemon_id']
+                ];
+            }
+
+            file_put_contents(self::STATS_JSON, json_encode($toWrite, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
         }
 
         $content = file_get_contents(self::STATS_JSON);
         return json_decode($content, true);
     }
 
-    public static function getQuickMoves()
+    public static function getQuickMoves($force = false)
     {
-        if (!file_exists(self::QUICK_MOVES_JSON)) {
-            file_put_contents(self::QUICK_MOVES_JSON, file_get_contents("https://pogoapi.net/api/v1/fast_moves.json"));
+        if (!file_exists(self::QUICK_MOVES_JSON) || $force) {
+            $read = file_get_contents("https://pogoapi.net/api/v1/fast_moves.json");
+            $read = json_decode($read, true);
+
+            $toWrite = [];
+            foreach ($read as $item) {
+                $toWrite[$item['name']] = $item['type'];
+            }
+
+            file_put_contents(self::QUICK_MOVES_JSON, json_encode($toWrite, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
         }
 
         $content = file_get_contents(self::QUICK_MOVES_JSON);
         return json_decode($content, true);
     }
 
-    public static function getChargeMoves()
+    public static function getChargeMoves($force = false)
     {
-        if (!file_exists(self::CHARGE_MOVES_JSON)) {
-            file_put_contents(self::CHARGE_MOVES_JSON, file_get_contents("https://pogoapi.net/api/v1/charged_moves.json"));
+        if (!file_exists(self::CHARGE_MOVES_JSON) || $force) {
+            $read = file_get_contents("https://pogoapi.net/api/v1/charged_moves.json");
+            $read = json_decode($read, true);
+
+            $toWrite = [];
+            foreach ($read as $item) {
+                $toWrite[$item['name']] = $item['type'];
+            }
+
+            file_put_contents(self::CHARGE_MOVES_JSON, json_encode($toWrite, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
         }
 
         $content = file_get_contents(self::CHARGE_MOVES_JSON);
         return json_decode($content, true);
     }
 
-    public static function getCpMultiplier()
+    public static function getCurrentPkmMoves($force = false)
     {
-        if (!file_exists(self::CP_MULTIPLIER_JSON)) {
-            file_put_contents(self::CP_MULTIPLIER_JSON, file_get_contents("https://pogoapi.net/api/v1/cp_multiplier.json"));
+        if (!file_exists(self::CURRENT_PKM_MOVES_JSON) || $force) {
+            $read = file_get_contents("https://pogoapi.net/api/v1/current_pokemon_moves.json");
+            $read = json_decode($read, true);
+
+            $toWrite = [];
+            foreach ($read as $item) {
+
+                $name = $item['pokemon_name'];
+
+                if (in_array($item['form'], self::ALLOWED_FORMS)) {
+                    $name = $item['form'] . " " . $item['pokemon_name'];
+                }
+
+                $toWrite[$name]['quick'] = array_merge($item['fast_moves'], $item['elite_fast_moves']);
+                $toWrite[$name]['charge'] = array_merge($item['charged_moves'], $item['elite_charged_moves']);
+            }
+
+            file_put_contents(self::CURRENT_PKM_MOVES_JSON, json_encode($toWrite, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
         }
 
-        $content = file_get_contents(self::CP_MULTIPLIER_JSON);
+        $content = file_get_contents(self::CURRENT_PKM_MOVES_JSON);
         return json_decode($content, true);
-    }
-
-    private static function compareHashes()
-    {
     }
 }
