1fe391c5e0948d98ff902d76e9a9dfbb5b355d61
Paul Richard Pereire Martins dos Anjos <paulrpm@gmail.com>
2021-09-17 19:13:48 -0300

    code cleanup on main controller

--- app/Controllers/MainController.php
+++ app/Controllers/MainController.php
@@ -4,14 +4,18 @@ namespace App\Controllers;
 
 use App\Templates;
 use App\Utils\JsonUtil;
+use App\Utils\GeneralUtils;
 
 class MainController
 {
     protected $jsonUtil;
 
+    protected $generalUtil;
+
     public function __construct()
     {
         $this->jsonUtil = new JsonUtil();
+        $this->generalUtil = new GeneralUtils();
     }
 
     /*
@@ -19,12 +23,12 @@ class MainController
      */
     public function jsonUpdate()
     {
+        $this->jsonUtil->getQuickMoves(true);
+        $this->jsonUtil->getChargeMoves(true);
         $this->jsonUtil->getTypeEffectiveness(true);
         $this->jsonUtil->getMegaPokemons(true);
         $this->jsonUtil->getType(true);
         $this->jsonUtil->getStats(true);
-        $this->jsonUtil->getQuickMoves(true);
-        $this->jsonUtil->getChargeMoves(true);
         $this->jsonUtil->getCurrentPkmMoves(true);
     }
 
@@ -58,7 +62,7 @@ class MainController
                 'quick' => $jsonCurrentMoves[$pokemonName]['quick'],
                 'charge' => $jsonCurrentMoves[$pokemonName]['charge']
             ],
-            'imgurl' => $this->formatImgUrl($jsonStats[$pokemonName]['id'], $pokemonName)
+            'imgurl' => $this->generalUtil->formatImgUrl($jsonStats[$pokemonName]['id'], $pokemonName)
         ]);
     }
 
@@ -69,61 +73,15 @@ class MainController
     {
         $name = str_replace('_', ' ', $name);
 
-        $types = $this->jsonUtil->getTypeEffectiveness();
-
-        $goodAgainst = [];
-        $weakAgainst = [];
-        $moveType = '';
-
-        switch ($type) {
-            case 'quick':
-                $quickMovesJson = $this->jsonUtil->getQuickMoves();
-
-                foreach (array_keys($quickMovesJson) as $quickMove) {
-                    if ($quickMove === $name) {
-                        $moveType = $quickMovesJson[$quickMove];
-                        foreach ($types[$quickMovesJson[$quickMove]] as $key => $value)
-                        {
-                            if ($value > 1) {
-                                $goodAgainst[] = $key;
-                            }
-                            if ($value < 1) {
-                                $weakAgainst[] = $key;
-                            }
-                        }
-                    }
-                }
-
-                break;
-
-            case 'charge1' || 'charge2':
-                $chargeMovesJson = $this->jsonUtil->getChargeMoves();
-
-                foreach (array_keys($chargeMovesJson) as $chargeMove) {
-                    if ($chargeMove === $name) {
-                        $moveType = $chargeMovesJson[$chargeMove];
-                        foreach ($types[$chargeMovesJson[$chargeMove]] as $key => $value)
-                        {
-                            if ($value > 1) {
-                                $goodAgainst[] = $key;
-                            }
-                            if ($value < 1) {
-                                $weakAgainst[] = $key;
-                            }
-                        }
-                    }
-                }
-
-                break;
-        }
+        $moveData = ($type == 'quick') ? $this->jsonUtil->getQuickMoves() : $this->jsonUtil->getChargeMoves();
 
         $response = [
-            'type' => strtoupper($moveType),
-            'weakAgainst' => $weakAgainst,
-            'goodAgainst' => $goodAgainst
+            'type' => strtoupper($moveData[$name]['type']),
+            'weakAgainst' => $moveData[$name]['weakAgainst'],
+            'goodAgainst' => $moveData[$name]['goodAgainst']
         ];
         
-        $url = "https://pokemongo.fandom.com/wiki/" . str_replace(' ', '_', $name);
+        /*$url = "https://pokemongo.fandom.com/wiki/" . str_replace(' ', '_', $name);
         
         $file = file_get_contents($url);
 
@@ -142,7 +100,7 @@ class MainController
 		$moveData = ($type == 'quick') ?  [ $moveData[3], $moveData[4] ] : [ $moveData[2], $moveData[3] ] ;
         
         $response['power'] = $moveData[0];
-        $response['energy'] = $moveData[1];
+        $response['energy'] = $moveData[1];*/
 
         echo json_encode($response);
 
@@ -190,97 +148,6 @@ class MainController
         ];
 
         new Templates('reader.html', $args);
-    }
-
-    /*
-     * This function is responsible for generating the pokemon database files for the pure front end version of this app
-     */
-    public function jsBuilderPokeData()
-    {
-        $stats = $this->jsonUtil->getStats();
-        $types = $this->jsonUtil->getType();
-        $currentMoves = $this->jsonUtil->getCurrentPkmMoves();
-
-        $jsDB = "var pokeDB = {\n";
-
-        foreach ($stats as $name => $pkm) {
-            $defense_data['vulnerable_to'] = $currentMoves[$name]['vulnerable_to'];
-            $defense_data['resistant_to'] = $currentMoves[$name]['resistant_to'];
-
-            $pokeData['id'] = str_pad($pkm['id'], 3, '0', 0);
-            $pokeData['imgurl'] = $this->formatImgUrl($pkm['id'], $name);
-            unset($pkm['id']);
-            $pokemonType = explode("/", $types[$name]);
-            $pokeData['stats'] = $pkm;
-            $pokeData['type'] = $pokemonType;
-            $pokeData['name'] = $name;
-            $pokeData['moveset']['quick'] = $currentMoves[$name]['quick'];
-            $pokeData['moveset']['charge'] = $currentMoves[$name]['charge'];
-            $pokeData['defense_data'] = $defense_data;
-
-            $jsDB .= "\"$name\": " . json_encode($pokeData, JSON_PRETTY_PRINT) . ",\n";
-        }
-
-        $jsDB .= "}";
-
-        file_put_contents('includes/files/db_pokedata.js', $jsDB);
-
-    }
-
-    /*
-     * This function is responsible for generating the pokemon database files for the pure front end version of this app
-     */
-    public function jsBuilderQuick()
-    {
-
-        $quickMoves = $this->jsonUtil->getQuickMoves();
-
-        $jsDB = "var quickMoveDB = {\n";
-
-        foreach ($quickMoves as $name => $quickMove) {
-            $getMove = $this->getMove($name, 'quick');
-
-            $moveData['type'] = $getMove['type'];
-            $moveData['weakAgainst'] = $getMove['weakAgainst'];
-            $moveData['goodAgainst'] = $getMove['goodAgainst'];
-            $moveData['power'] = $getMove['power'];
-            $moveData['energy'] = $getMove['energy'];
-
-            $jsDB .= "\"$name\": " . json_encode($moveData, JSON_PRETTY_PRINT) . ",\n";
-        }
-
-        $jsDB .= "}";
-
-        file_put_contents('includes/files/db_quick.js', $jsDB);
-
-    }
-
-    /*
-     * This function is responsible for generating the pokemon database files for the pure front end version of this app
-     */
-    public function jsBuilderCharge()
-    {
-
-        $chargeMoves = $this->jsonUtil->getChargeMoves();
-
-        $jsDB = "var chargeMoveDB = {\n";
-
-        foreach ($chargeMoves as $name => $chargeMove) {
-            $getMove = $this->getMove($name, 'charge1');
-
-            $moveData['type'] = $getMove['type'];
-            $moveData['weakAgainst'] = $getMove['weakAgainst'];
-            $moveData['goodAgainst'] = $getMove['goodAgainst'];
-            $moveData['power'] = $getMove['power'];
-            $moveData['energy'] = $getMove['energy'];
-
-            $jsDB .= "\"$name\": " . json_encode($moveData, JSON_PRETTY_PRINT) . ",\n";
-        }
-
-        $jsDB .= "}";
-
-        file_put_contents('includes/files/db_charge.js', $jsDB);
-
     }
 
     public function teamAssembler()
@@ -343,42 +210,5 @@ class MainController
         sort($names);
 
         return $names;
-    }
-    
-
-    private function formatImgUrl($id, $name)
-    {
-        $imgUrl = $id;
-
-        $name = explode(" ", $name);
-
-        if (sizeof($name) == 1) {
-            return $imgUrl;
-        }
-
-        $form = $name[0];
-
-        switch ($form)
-        {
-            case "Galarian":
-                $formattedName = strtolower($name[1]) . '-galar';
-                $pkm = $this->jsonUtil->getPokeApiJson($formattedName);
-                $imgUrl = is_numeric($pkm['id']) ? $pkm['id'] : '';
-                break;
-
-            case "Alola":
-                $formattedName = strtolower($name[1]) . '-alola';
-                $pkm = $this->jsonUtil->getPokeApiJson($formattedName);
-                $imgUrl = is_numeric($pkm['id']) ? $pkm['id'] : '';
-                break;
-
-            case "Shadow":
-                break;
-
-            default:
-                $imgUrl = $id . '-' . strtolower($form);
-        }
-
-        return $imgUrl;
     }
 }
