93ca538f0fe03e5d056761d49e01095728dfd7c2
Paul Richard <paulrpm@gmail.com>
2025-04-29 11:47:00 -0300

    feat: updating validation to toxtricity

--- app/v2/JsonUtil.php
+++ app/v2/JsonUtil.php
@@ -117,7 +117,7 @@ class JsonUtil {
                 
                 $name = (isset($pokemon->form)) ? $pokemon->form : $pokemon->pokemonId;
                 $name = $this->formatPokemonName($name);
-                if (in_array($name, [ "Oricorio", "Gourgeist", "Darmanitan", "Thundurus", "Tornadus", "Landorus", "Enamorus", "Meloetta", "Giratina", "Morpeko" ])) {
+                if (in_array($name, [ "Oricorio", "Gourgeist", "Darmanitan", "Thundurus", "Tornadus", "Landorus", "Enamorus", "Meloetta", "Giratina", "Morpeko", "Toxtricity" ])) {
                     continue;
                 }
 
@@ -398,6 +398,7 @@ class JsonUtil {
             preg_match("/(giratina)/", $pkmName) ||
             preg_match("/(tauros paldea)/", $pkmName) ||
             preg_match("/(morpeko)/", $pkmName) ||
+            preg_match("/(toxtricity)/", $pkmName) ||
             str_contains($pkmName, "deoxys") || str_contains($pkmName, "darmanitan") || str_contains($pkmName, "tornadus") || str_contains($pkmName, "thundurus") || str_contains($pkmName, "landorus") || str_contains($pkmName, "enamorus") || str_contains($pkmName, "meloetta" || str_contains($pkmName, "tapu"))) {
             $pkmName = str_replace(" ", "-", $pkmName);
         }
